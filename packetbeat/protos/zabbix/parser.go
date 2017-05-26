6698be93b4bc37febde787e7bb4d4e6c978c9bba
XueBing <xuebing1110@gmail.com>
2017-05-26 17:06:38 +0800

    fix bug:can't pub zabbix item

--- packetbeat/protos/zabbix/parser.go
+++ packetbeat/protos/zabbix/parser.go
@@ -142,7 +142,7 @@ func (p *parser) parse() (*message, error) {
 
 	//get reponse body
 	if msg.IsRequest {
-		msg.item = string(bytes.TrimSuffix(buf, '\n'))
+		msg.item = buf[:bufCap-1]
 		logp.Info("get zabbix request:%s", msg.item)
 	} else {
 		logp.Info("get zabbix response...")
